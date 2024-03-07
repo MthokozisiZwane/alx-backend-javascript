749ee54dd1349f943e8b8efe4980db540237a2a7
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-03-07 14:29:07 +0200

    es6 advanced

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,4 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
-  
-    for (const department in reportWithIterator.allEmployees) {
-      result += reportWithIterator.allEmployees[department].join(' | ');
-  
-      if (department !== Object.keys(reportWithIterator.allEmployees)[Object.keys(reportWithIterator.allEmployees).length - 1]) {
-        result += '\n';
-      }
-    }
-  
-    return result;
-  }
-  
+    const employeeNames = reportWithIterator.map(employee => employee); // Get a list of employee names
+    return employeeNames.join(' | ');
+  } 
