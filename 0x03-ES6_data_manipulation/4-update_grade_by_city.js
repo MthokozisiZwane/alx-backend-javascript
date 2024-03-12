6172d9d2697861cef641f2bfb287117a76697eb8
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-03-12 14:55:00 -0700

    data manipulation es6

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,11 @@
 function updateStudentGradeByCity(students, city, newGrades) {
-  return students.map((student) => {
-    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-    const grade = gradeObj ? gradeObj.grade : 'N/A';
-    return { ...student, grade };
-  });
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
+    });
 }
 
 export default updateStudentGradeByCity;
