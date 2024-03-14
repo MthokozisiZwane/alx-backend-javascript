52dc6bfc6ab67d506068aea3dd37d14a28d70e79
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-03-14 05:37:13 -0700

    es6 data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,16 @@
 function cleanSet(set, startString) {
   let result = '';
-  set.forEach((value) => {
+  set.forEach(value => {
     if (value.startsWith(startString)) {
       const trimmedValue = value.slice(startString.length);
-      result += `${trimmedValue}-`;
+      result += trimmedValue + '-';
     }
   });
-  return result.slice(0, -1);
+  if (result.length === 0) {
+    return '';
+  } else {
+    return result.slice(0, -1);
+  }
 }
 
 export default cleanSet;
