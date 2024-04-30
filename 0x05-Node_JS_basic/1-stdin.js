981dedecfb3af22dabcf6bb27298f3fc3ab1d7cb
MthokozisiZwane <mhokozisi.zwane1999@gmail.com>
2024-04-30 14:30:51 +0000

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+/**
+ * Reads input from the command line and interacts with the user.
+ */
+function userInteraction() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    if (name) {
+      console.log(`Your name is: ${name}`);
+    } else {
+      console.log('Your name is: <undefined>');
+    }
+    process.stdout.write('\nThis important software is now closing\n');
+    process.exit();
+  });
+}
+
+userInteraction();
