63499c5e239f803ea53fb8737680cb37100495f5
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-05-05 01:52:23 +0200

    9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Cart page', () => {
+  it('Correct status code when :id is a number?', (done) => {
+    request('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct status code when :id is NOT a number (=> 404)?', (done) => {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+  it('Other?', (done) => {
+    done();
+  });
+});
