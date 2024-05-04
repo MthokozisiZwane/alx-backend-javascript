aa8bb83549e0303f1d66447b83aa1c5601e53a4a
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-05-05 00:47:34 +0200

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,30 +1,14 @@
 const { expect } = require('chai');
-const getPaymentTokenFromAPI = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', function () {
-    it('should return a resolved promise with the correct data when success is true', function (done) {
-        // Call the function with success set to true
-        getPaymentTokenFromAPI(true)
-            .then(response => {
-                // Verify that the response object contains the correct data
-                expect(response).to.deep.equal({ data: 'Successful response from the API' });
-                done();
-            })
-            .catch(error => {
-                done(error);
-            });
-    });
+const getPaymentTokenFromApi = require('./6-payment_token');
 
-    it('should return an unresolved promise when success is false', function (done) {
-        // Call the function with success set to false
-        getPaymentTokenFromAPI(false)
-            .then(response => {
-                done(new Error('Promise should not be resolved'));
-            })
-            .catch(error => {
-                // Verify that the promise is rejected
-                expect(error).to.be.undefined;
-                done();
-            });
-    });
+describe('getPaymentTokenFromApi', () => {
+  it('Calls the function with success set to true', (done) => {
+    getPaymentTokenFromApi(true)
+      .then((res) => {
+        expect(res).to.include({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => done(error));
+  });
 });
