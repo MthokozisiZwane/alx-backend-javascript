44dd6ee24fd124c4f58702dafeb405b75ff4a8f0
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-05-05 16:25:29 +0200

    9. Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,22 +1,17 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api');
-
-chai.use(chaiHttp);
-const { expect } = chai;
+const request = require('request');
+const { expect } = require('chai');
 
 describe('Cart page', () => {
-  describe('GET /cart/:id', () => {
-    it('should return payment methods for a valid cart ID', async () => {
-      const res = await chai.request(app).get('/cart/12');
-      expect(res).to.have.status(200);
-      expect(res.text).to.equal('Payment methods for cart 12');
-    });
-
-    it('should return a 404 status code for an invalid cart ID', async () => {
-      const res = await chai.request(app).get('/cart/hello');
-      expect(res).to.have.status(404);
-      expect(res.text).to.equal('Cannot GET /cart/hello');
+  it('Correct status code when :id is a number?', (done) => {
+    request('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
     });
   });
-});
+
+  it('Correct status code when :id is NOT a number (=> 404)?', (done) => {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
